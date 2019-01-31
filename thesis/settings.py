574e025c5c0be2500695be4541ef2df2327278c5
mharjoliee <mharjorie_sandel@dlsu.edu.ph>
2019-01-31 15:15:43 +0800

    I love kayle so much

--- thesis/settings.py
+++ thesis/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
             'ENGINE': 'django.db.backends.mysql',
             'NAME': 'mattress',
             'USER': 'root',
-            'PASSWORD': 'root',
+            'PASSWORD': '',
             'HOST': '127.0.0.1',
             'PORT': '3306',
     }
